96028d455aa483f49f3877a13b31ed8d6493d049
Anushka121 <aanshusoni56@gmail.com>
2020-04-10 20:32:37 +0530

    spring 1.2

---
+++ Employee_Details 1.2/Employee_Details 1.2/src/main/java/com/cg/app/EmployeeMain.java
@@ -0,0 +1,33 @@
+package com.cg.app;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+
+
+public class EmployeeMain {
+
+    public static void main(String[] args) {
+    	
+    	EmployeeMain empmain=new EmployeeMain();
+    	
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        
+        Class configurationClass=JavaConfig.class;
+        context.register(configurationClass);
+        context.refresh();
+       context.registerShutdownHook();
+       
+        
+        Employee employee = context.getBean(Employee.class);
+        SBU sbu=context.getBean(SBU.class);
+        employee.setSbulist(sbu);
+        employee.display();
+        
+    
+    
+    }
+    
+    
+}
---
+++ Employee_Details 1.2/Employee_Details 1.2/src/main/java/com/cg/app/JavaConfig.java
@@ -0,0 +1,14 @@
+package com.cg.app;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan("com.cg.app")
+@PropertySource("classpath:employee.properties")
+public class JavaConfig {
+
+	
+ 
+
+}
